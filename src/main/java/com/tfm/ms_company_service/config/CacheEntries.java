498391554c76ae771133fbe7b73d19d03e3e515e
MarioTriMar <mario.t.m2001@hotmail.com>
2025-07-05 14:53:55 +0200

    cache redis

--- src/main/java/com/tfm/ms_company_service/config/CacheEntries.java
+++ src/main/java/com/tfm/ms_company_service/config/CacheEntries.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @Component
 @Slf4j
 public class CacheEntries {
+    /*
     @Autowired
     private CacheManager cacheManager;
 
@@ -29,4 +30,6 @@ public class CacheEntries {
             throw new IllegalStateException("Cache is not a Caffeine cache");
         }
     }
+
+     */
 }
