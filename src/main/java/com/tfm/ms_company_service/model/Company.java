8b9e79ff3710dd158d8266a252ff50c921b1ccf4
MarioTriMar <mario.t.m2001@hotmail.com>
2025-07-05 15:09:21 +0200

    cache redis

--- src/main/java/com/tfm/ms_company_service/model/Company.java
+++ src/main/java/com/tfm/ms_company_service/model/Company.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 @Data
 @Document(value="companyEntity")
-public class Company {
+public class Company implements Serializable {
 
     @Id
     private String id;
